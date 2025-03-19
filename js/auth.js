839062236d3a13f8ae7d92474ded8238c922958f
Yun-Jinwoo <yjw3480@naver.com>
2025-03-19 19:11:31 +0900

    ✨ Activate login button when inputs are valid

--- js/auth.js
+++ js/auth.js
@@ -2,15 +2,19 @@ const emailInput = document.querySelector("#input-email");
 const wrongEmail = document.createElement("span");
 
 function wrongEmailEvent(event) {
-  event.target.classList.add("warning");
   if (event.target.value === "") {
+    event.target.classList.add("warning");
+    event.target.classList.remove("done");
     wrongEmail.textContent = "이메일을 입력해주세요.";
   } else if (!event.target.checkValidity()) {
+    event.target.classList.add("warning");
+    event.target.classList.remove("done");
     wrongEmail.textContent = "잘못된 이메일 형식입니다.";
   } else {
+    event.target.classList.remove("warning");
+    event.target.classList.add("done");
     wrongEmail.textContent = "";
   }
-
   emailInput.parentElement.appendChild(wrongEmail);
   if (wrongEmail.textContent === "") {
     emailInput.parentElement.removeChild(wrongEmail);
@@ -23,15 +27,19 @@ const passwordInput = document.querySelector("#input-password");
 const wrongPassword = document.createElement("span");
 
 function wrongPasswordEvent(event) {
-  event.target.classList.add("warning");
   if (event.target.value === "") {
+    event.target.classList.add("warning");
+    event.target.classList.remove("done");
     wrongPassword.textContent = "비밀번호를 입력해주세요.";
   } else if (event.target.value.length < 8) {
+    event.target.classList.add("warning");
+    event.target.classList.remove("done");
     wrongPassword.textContent = "비밀번호를 8자 이상 입력해주세요.";
   } else {
+    event.target.classList.remove("warning");
+    event.target.classList.add("done");
     wrongPassword.textContent = "";
   }
-
   passwordInput.parentElement.appendChild(wrongPassword);
   if (wrongPassword.textContent === "") {
     passwordInput.parentElement.removeChild(wrongPassword);
@@ -39,3 +47,21 @@ function wrongPasswordEvent(event) {
 }
 
 passwordInput.addEventListener("focusout", wrongPasswordEvent);
+
+const loginButton = document.querySelector(".login-page .submit-button");
+loginButton.disabled = true;
+function activateButton() {
+  if (
+    emailInput.classList.contains("done") &&
+    passwordInput.classList.contains("done")
+  ) {
+    loginButton.disabled = false;
+  } else {
+    loginButton.disabled = true;
+  }
+}
+
+emailInput.addEventListener("input", activateButton);
+passwordInput.addEventListener("input", activateButton);
+emailInput.addEventListener("focusout", activateButton);
+passwordInput.addEventListener("focusout", activateButton);
