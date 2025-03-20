c5c40fdc4b9829795f1662e5f4e2a93e404731c3
Yun-Jinwoo <yjw3480@naver.com>
2025-03-21 03:00:58 +0900

    ♻️ Reorganize DOM element
    Refactor code by moving declarations to the top and structuring similar elements together

--- js/login.js
+++ js/login.js
@@ -4,10 +4,21 @@ import {
   activateButton,
   redirectToPage,
 } from "./common.js";
-/* email 에러 메시지 처리 */
+
 const emailInput = document.querySelector("#input-email");
+const passwordInput = document.querySelector("#input-password");
+const loginButton = document.querySelector(".login-page .submit-button");
+const loginForm = document.querySelector(".login-page .form-container");
+
 const wrongEmail = document.createElement("span");
+const wrongPassword = document.createElement("span");
+
+const inputArray = [emailInput, passwordInput];
+
 wrongEmail.classList.add("error-message");
+wrongPassword.classList.add("error-message");
+
+loginButton.disabled = true;
 
 function validateEmail(input) {
   if (input.value === "") {
@@ -17,18 +28,6 @@ function validateEmail(input) {
   }
 }
 
-emailInput.addEventListener("focusout", () =>
-  wrongInput(emailInput, wrongEmail, validateEmail)
-);
-emailInput.addEventListener("input", () =>
-  wrongInput(emailInput, wrongEmail, validateEmail)
-);
-
-/* password 에러 메시지 처리 */
-const passwordInput = document.querySelector("#input-password");
-const wrongPassword = document.createElement("span");
-wrongPassword.classList.add("error-message");
-
 function validatePassword(input) {
   if (input.value === "") {
     return "비밀번호를 입력해주세요.";
@@ -37,6 +36,15 @@ function validatePassword(input) {
   }
 }
 
+/* email 에러 메시지 처리 */
+emailInput.addEventListener("focusout", () =>
+  wrongInput(emailInput, wrongEmail, validateEmail)
+);
+emailInput.addEventListener("input", () =>
+  wrongInput(emailInput, wrongEmail, validateEmail)
+);
+
+/* password 에러 메시지 처리 */
 passwordInput.addEventListener("focusout", () =>
   wrongInput(passwordInput, wrongPassword, validatePassword)
 );
@@ -45,10 +53,6 @@ passwordInput.addEventListener("input", () =>
 );
 
 /* email, password에 유효한 값이 입력되면 로그인 페이지 버튼 활성화 */
-const loginButton = document.querySelector(".login-page .submit-button");
-const inputArray = [emailInput, passwordInput];
-loginButton.disabled = true;
-
 inputArray.forEach((input) => {
   input.addEventListener("input", () =>
     activateButton(loginButton, inputArray)
@@ -62,8 +66,6 @@ inputArray.forEach((input) => {
 });
 
 /* 로그인 버튼 클릭시 items 페이지로 이동 */
-const loginForm = document.querySelector(".login-page .form-container");
-
 loginForm.addEventListener("submit", (event) =>
   redirectToPage(event, "/items.html")
 );
--- js/signup.js
+++ js/signup.js
@@ -5,10 +5,31 @@ import {
   redirectToPage,
 } from "./common.js";
 
-/* email 에러 메시지 처리 */
 const emailInput = document.querySelector("#input-email");
+const nicknameInput = document.querySelector("#input-nickname");
+const passwordInput = document.querySelector("#input-password");
+const passwordCheckInput = document.querySelector("#input-password-check");
+const signupButton = document.querySelector(".signup-page .submit-button");
+const signupForm = document.querySelector(".signup-page .form-container");
+
 const wrongEmail = document.createElement("span");
+const wrongNickname = document.createElement("span");
+const wrongPassword = document.createElement("span");
+const wrongpasswordCheck = document.createElement("span");
+
+const inputArray = [
+  emailInput,
+  nicknameInput,
+  passwordInput,
+  passwordCheckInput,
+];
+
 wrongEmail.classList.add("error-message");
+wrongNickname.classList.add("error-message");
+wrongPassword.classList.add("error-message");
+wrongpasswordCheck.classList.add("error-message");
+
+signupButton.disabled = true;
 
 function validateEmail(input) {
   if (input.value === "") {
@@ -18,36 +39,11 @@ function validateEmail(input) {
   }
 }
 
-emailInput.addEventListener("focusout", () =>
-  wrongInput(emailInput, wrongEmail, validateEmail)
-);
-emailInput.addEventListener("input", () =>
-  wrongInput(emailInput, wrongEmail, validateEmail)
-);
-
-/* nickname 에러 메시지 처리 */
-
-const nicknameInput = document.querySelector("#input-nickname");
-const wrongNickname = document.createElement("span");
-wrongNickname.classList.add("error-message");
-
 function validateNickname(input) {
   if (input.value === "") {
     return "닉네임을 입력해주세요.";
   }
 }
-
-nicknameInput.addEventListener("focusout", () =>
-  wrongInput(nicknameInput, wrongNickname, validateNickname)
-);
-nicknameInput.addEventListener("input", () =>
-  wrongInput(nicknameInput, wrongNickname, validateNickname)
-);
-
-/* password 에러 메시지 처리 */
-const passwordInput = document.querySelector("#input-password");
-const wrongPassword = document.createElement("span");
-wrongPassword.classList.add("error-message");
 
 function validatePassword(input) {
   if (input.value === "") {
@@ -56,18 +52,6 @@ function validatePassword(input) {
     return "비밀번호를 8자 이상 입력해주세요.";
   }
 }
-
-passwordInput.addEventListener("focusout", () =>
-  wrongInput(passwordInput, wrongPassword, validatePassword)
-);
-passwordInput.addEventListener("input", () =>
-  wrongInput(passwordInput, wrongPassword, validatePassword)
-);
-
-/* password-check 에러 메시지 처리 */
-const passwordCheckInput = document.querySelector("#input-password-check");
-const wrongpasswordCheck = document.createElement("span");
-wrongpasswordCheck.classList.add("error-message");
 
 function wrongPasswordCheckEvent() {
   if (passwordCheckInput.value === "") {
@@ -87,21 +71,37 @@ function wrongPasswordCheckEvent() {
     passwordCheckInput.parentElement.removeChild(wrongpasswordCheck);
   }
 }
-/* password가 변경되어도 즉시 password-check과 비교 */
-passwordInput.addEventListener("input", wrongPasswordCheckEvent);
+
+/* email 에러 메시지 처리 */
+emailInput.addEventListener("focusout", () =>
+  wrongInput(emailInput, wrongEmail, validateEmail)
+);
+emailInput.addEventListener("input", () =>
+  wrongInput(emailInput, wrongEmail, validateEmail)
+);
+
+/* nickname 에러 메시지 처리 */
+nicknameInput.addEventListener("focusout", () =>
+  wrongInput(nicknameInput, wrongNickname, validateNickname)
+);
+nicknameInput.addEventListener("input", () =>
+  wrongInput(nicknameInput, wrongNickname, validateNickname)
+);
+
+/* password 에러 메시지 처리 */
+passwordInput.addEventListener("focusout", () =>
+  wrongInput(passwordInput, wrongPassword, validatePassword)
+);
+passwordInput.addEventListener("input", () =>
+  wrongInput(passwordInput, wrongPassword, validatePassword)
+);
+
+/* password-check 에러 메시지 처리 */
+passwordInput.addEventListener("input", wrongPasswordCheckEvent); // password가 변경되어도 즉시 password-check과 비교
 passwordCheckInput.addEventListener("focusout", wrongPasswordCheckEvent);
 passwordCheckInput.addEventListener("input", wrongPasswordCheckEvent);
 
 /* email, nickname, password, password-check에 유효한 값이 입력되면 회원가입 페이지 버튼 활성화 */
-const signupButton = document.querySelector(".signup-page .submit-button");
-const inputArray = [
-  emailInput,
-  nicknameInput,
-  passwordInput,
-  passwordCheckInput,
-];
-signupButton.disabled = true;
-
 inputArray.forEach((input) => {
   input.addEventListener("input", () =>
     activateButton(signupButton, inputArray)
@@ -115,8 +115,6 @@ inputArray.forEach((input) => {
 });
 
 /* 회원가입 버튼 클릭시 로그인 페이지로 이동 */
-const signupForm = document.querySelector(".signup-page .form-container");
-
 signupForm.addEventListener("submit", (event) =>
   redirectToPage(event, "/login.html")
 );
