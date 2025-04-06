d19ea64dca0f947c69c9a882d64c461a359977bd
Yun-Jinwoo <yjw3480@naver.com>
2025-04-06 16:46:59 +0900

    ✨ feat: 유효하지 않은 경로에 대한 NotFound 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Market from "./pages/Market";
 import Community from "./pages/Community";
 import AddItem from "./pages/AddItem";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/items" element={<Market />} />
       <Route path="/boards" element={<Community />} />
       <Route path="/additem" element={<AddItem />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
