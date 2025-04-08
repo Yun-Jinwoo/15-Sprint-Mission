199e67dc613e1e1cfda3bc853429a3e9debaeac4
Yun-Jinwoo <yjw3480@naver.com>
2025-04-08 20:49:00 +0900

    ✨ feat: BestItems에서 좋아요 상위 4개 상품 표시

--- src/pages/Market/BestItems/BestItems.jsx
+++ src/pages/Market/BestItems/BestItems.jsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
+import { getProducts } from "../../../api";
 import "./BestItems.css";
 import Item from "../Item/Item";
 
 const BestItems = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function getBestItems() {
+      const data = await getProducts({ pageSize: 4, orderBy: "favorite" });
+      setItems(data.list);
+    }
+
+    getBestItems();
+  }, []);
   return (
     <>
       <div className="BestItems">
         <p className="title">베스트 상품</p>
         <div className="item-list">
-          <Item />
+          {items.map((item) => {
+            return <Item key={item.id} item={item} />;
+          })}
         </div>
       </div>
     </>
