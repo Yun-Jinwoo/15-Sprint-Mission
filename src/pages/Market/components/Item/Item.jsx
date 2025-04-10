5a6b0a0c98744c627806a13252cb689c6fd6f787
Yun-Jinwoo <yjw3480@naver.com>
2025-04-11 01:37:38 +0900

    🎨 style: Item 컴포넌트 스타일 변경
    - 상품 설명을 이름으로 변경
    - size 클래스 부모 요소로 변경

--- src/pages/Market/components/Item/Item.jsx
+++ src/pages/Market/components/Item/Item.jsx
@@ -2,19 +2,19 @@ import "./Item.css";
 import heart from "../../../../../src/assets/images/heart.svg";
 import fallback from "../../../../../src/assets/images/fallback.png";
 const Item = ({ item, size }) => {
-  const { images, name, description, price, favoriteCount } = item;
+  const { images, name, price, favoriteCount } = item;
   return (
-    <div className="Item">
+    <div className={`Item size-${size}`}>
       <img
         src={images && images.length > 0 ? images[0] : fallback}
         alt={name}
-        className={`item-img size-${size}`}
+        className="item-img"
         onError={(e) => {
           e.target.src = fallback;
         }}
       />
       <div className="text-section">
-        <p className="item-description">{description}</p>
+        <p className="item-name">{name}</p>
         <p className="item-price">{price}원</p>
         <div className="favorite">
           <img src={heart} alt="좋아요 아이콘" />
