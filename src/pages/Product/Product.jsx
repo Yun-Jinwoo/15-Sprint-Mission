8def5defbeefe070a6bdeaa1250f925083bf2591
Yun-Jinwoo <yjw3480@naver.com>
2025-05-10 16:22:39 +0900

    ✨ feat: Product 컴포넌트 구현

---
+++ src/pages/Product/Product.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import ProductInfo from "./components/ProductInfo/ProductInfo";
+
+const Product = () => {
+  return (
+    <>
+      <Header />
+      <ProductInfo />
+    </>
+  );
+};
+
+export default Product;
